6c49b309d839c7a936485282e5250008c7e513a8
Scott  Bachman <scott.bachman@gmail.com>
2018-09-05 21:21:07 -0400

    Template and start of disenchantment

---
+++ template.js
@@ -0,0 +1,26 @@
+undum.game.id = "e0b6ad04-e0a6-4be6-80ff-f5ea391809d3";
+undum.game.version = "1.0";
+
+undum.game.start = "start";
+
+undum.game.situations = {
+  start: new undum.SimpleSituation(marked(`
+# The Cabin
+    
+The front of the small cabin is entirely 
+occupied with navigational instruments, a 
+radar display, and radios for calling 
+back to shore. Along each side runs a 
+bench with faded blue vinyl cushions, 
+which can be lifted to reveal the storage 
+space underneath. A glass case against 
+the wall contains several fishing rods.
+
+Scratched windows offer a view of the 
+surrounding bay, and there is a door 
+south to the deck. A sign taped to one 
+wall announces the menu of tours offered 
+by the Yakutat Charter Boat Company.`))
+};
+
+
